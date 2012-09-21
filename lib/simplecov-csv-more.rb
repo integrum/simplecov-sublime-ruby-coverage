d34aaa71b5f943b032c4cba9125dee51d9c5a81a
Jade Meskill and Roy van de Water <dev+jade+roy@integrumtech.com>
2012-09-21 11:25:30 -0700

    Make more-er

--- lib/simplecov-csv-more.rb
+++ lib/simplecov-csv-more.rb
@@ -2,35 +2,40 @@ class SimpleCov::Formatter::CSVMoreFormatter
   RESULT_FILE_NAME = "more-results.csv"
 
   def format( result )
-    data   = []
-    data << ["File", "Line Number", "Covered"]
+
+    @path_relativizer = Hash.new{|h,base|
+      h[base] = Pathname.new(base).cleanpath.to_s.gsub(%r{^\w:[/\\]}, "").gsub(/\./, "_").gsub(/[\\\/]/, "-") + ".html"
+    }
 
     result.files.each do |file|
-      file.lines.each do |line|
-        next if line.skipped?
+      data = []
+      data << ["Covered"]
 
-        data << [ shortened_filename(file), line.number, line.covered? ]
+      file.lines.each do |line|
+        data << [ line.covered? ]
+      end
+
+      csv = data.map{|row| row.join(',')}.join("\n")
+
+      result_file_path = File.join( SimpleCov.coverage_path, 'csv-more', relative_filename(shortened_filename(file)), "w" ) )
+      File.open( result_file_path ) do |file_result|
+        file_result.write csv
       end
     end
 
-    csv = data.map{|row| row.join(',')}.join("\n")
-
-    result_file_path = File.join( SimpleCov.coverage_path, SimpleCov::Formatter::CSVMoreFormatter.result_file_name )
-    File.open( result_file_path, "w" ) do |file_result|
-      file_result.write csv
-    end
-    puts "Coverage report generated for #{result.command_name} to #{result_file_path}"
-
-    return csv
+    puts "Coverage report generated for #{result.command_name} to #{File.join( SimpleCov.coverage_path, 'csv-more' )}"
   end
 
   private
+  def relative_filename(path)
+    @path_relativizer[path]
+  end
 
-    def shortened_filename(source_file)
-      source_file.filename.gsub(SimpleCov.root, '.')
-    end
+  def shortened_filename(source_file)
+    source_file.filename.gsub(SimpleCov.root, '.')
+  end
 
-    def self.result_file_name
-      SimpleCov::Formatter::CSVMoreFormatter::RESULT_FILE_NAME
-    end
+  def self.result_file_name
+    SimpleCov::Formatter::CSVMoreFormatter::RESULT_FILE_NAME
+  end
 end
